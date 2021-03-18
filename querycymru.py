5c4e9d3f17393fc9c53d25e6ca60ef2d098b2921
darrellmiller <darrellrhodesmiller@gmail.com>
2021-03-18 11:14:20 -0500

    Framework has been created, script will be written next

---
+++ querycymru.py
@@ -0,0 +1,7 @@
+# Louisiana State Police / Louisiana Cyber Investigators Alliance
+# QueryCymru: simple script that pulls indicators from text file and submits jobs to team cymru
+#
+# - Resources:
+#   -- https://team-cymru.com/
+
+print ("script will go here")
